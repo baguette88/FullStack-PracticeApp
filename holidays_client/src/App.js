a346ef2c66ffcb191871fd7f633420ff76c06d9d
jbrown7407 <jbrown7407@gmail.com>
2020-11-15 18:17:28 -0500

    SAFE1
    xx

---
+++ holidays_client/src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import NewForm from './components/NewForm.js'
+
+const baseURL = 'http://localhost:3003'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      holidays: []
+    }
+  
+    this.getHolidays = this.getHolidays.bind(this)
+    this.handleAddHoliday = this.handleAddHoliday.bind(this)
+  }
+
+  componentDidMount() {
+    this.getHolidays()
+  }
+
+  handleAddHoliday(holiday) {
+    this.setState({
+      holidays: this.state.holidays.concat(holiday)
+    })
+  }
+
+  getHolidays() {
+    fetch(baseURL + '/holidays')
+    .then(data => {
+      return data.json()
+    }).then(parsedData => {
+      this.setState({
+        holidays: parsedData
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Holidays!</h1>
+        <NewForm handleAddHoliday={this.handleAddHoliday} />
+        <table>
+          <tbody>
+            {
+              this.state.holidays.map(holiday => {
+                return (
+                  <tr>
+                    <td key={holiday._id}> {holiday.name} </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
